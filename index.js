d8992f674b758daeb0a91de18f63347520aeb3eb
neerajcode34 <109073148+neerajcode34@users.noreply.github.com>
2023-05-07 11:31:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+app.use(express.json());
+
+app.use(cors());
+
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
-const stripeRoute = require("./routes/stripe");
-const path = require("path");
+const stripeRoute = require('./routes/stripe');
 
-dotenv.config();
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("DB connection successful");
-        app.listen(process.env.PORT || 5002, () => {
-            console.log('listening on port');
-        });
-    })
-    .catch((err) => {
-        console.log(err);
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'), (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
     });
+  });
+}
 
-app.use(cors()); // add cors middleware
-app.use(express.json());
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
-
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html"),
-        function (err) {
-            res.status(500).send(err);
-        }
-    );
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
 });
+
 
 
 
