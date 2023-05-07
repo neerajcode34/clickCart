7dbdb3f83e1870810d30fed7c6b2b92f1b44471c
neerajcode34 <109073148+neerajcode34@users.noreply.github.com>
2023-05-07 10:41:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
