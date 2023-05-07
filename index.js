8615d1e5b8d52c242d57aab0884b99bcb88081c1
neerajcode34 <109073148+neerajcode34@users.noreply.github.com>
2023-05-07 08:57:31 +0530

    updated mongodb to connect before server start listening

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
-const productRoute = require('./routes/product')
-const cartRoute = require('./routes/cart')
-const orderRoute = require('./routes/order')
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 const stripeRoute = require("./routes/stripe");
 const path = require("path");
 
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(()=> console.log("DB connection successful")).catch((err)=>{
-    console.log(err);
-});
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("DB connection successful");
+        app.listen(process.env.PORT || 5002, () => {
+            console.log('listening on port');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.use(cors()); // add cors middleware
 app.use(express.json());
@@ -25,25 +31,18 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("/api/checkout",stripeRoute);
-
-
-// app.use(express.static(path.join(__dirname, './client/build'));
-
-// app.get("*",(req, res) => {
-// res.sendFile(path.join(__dirname, './client/build/index.html'));
-// })
+app.use("/api/checkout", stripeRoute);
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
+    res.sendFile(
+        path.join(__dirname, "./client/build/index.html"),
+        function (err) {
+            res.status(500).send(err);
+        }
+    );
 });
 
-app.listen(process.env.PORT || 5002, () => {
-    console.log('listening on port');
-});
+
+
+
